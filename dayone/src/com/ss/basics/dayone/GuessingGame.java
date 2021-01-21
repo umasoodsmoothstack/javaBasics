ee9e9ae352d614decd4cb04f320ebe7b2406f984
umasoodsmoothstack <77764817+umasoodsmoothstack@users.noreply.github.com>
2021-01-20 21:21:47 -0600

    Add files via upload

---
+++ dayone/src/com/ss/basics/dayone/GuessingGame.java
@@ -0,0 +1,10 @@
+package com.ss.basics.dayone;
+
+public class GuessingGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ dayone/src/com/ss/basics/dayone/dayone.java
@@ -0,0 +1,49 @@
+//Package
+package com.ss.basics.dayone;
+
+// Random and input 
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * @author Usman Masood
+ * Guess a number between 1 and 100
+ */
+public class dayone {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// Variables needed 
+		Random rand = new Random();
+		int numToGuess = rand.nextInt(100); // Generate a number
+		int numTries = 0; // Store number off tries
+		Scanner input = new Scanner(System.in); // Get input
+		int guess; // Store Guess
+		boolean win = false; // Win State
+		
+		// Run the loop while the win state is false
+		while (win == false) {
+		
+			System.out.println("Guess a number between 1 and 100");
+			guess = input.nextInt(); // User inputs number
+			numTries++; // Add one try
+			
+			// Guess 
+			if (guess - numToGuess <= 10 && guess - numToGuess >= -10) {
+				System.out.println("Congratulations, the number is " + numToGuess);
+				win = true;
+			} else {
+				System.out.println("Keep trying.");
+				if (numTries > 5) {
+					System.out.println("Sorry, the number is " + numToGuess);
+					System.exit(1);
+				}
+			}
+		}
+		
+		System.exit(1);
+	}
+
+}
